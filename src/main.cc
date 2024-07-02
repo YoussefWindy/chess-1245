05ee57145c1d0c5a6f3534e70d49ad5cd298a782
greygimple <greygimple@gmail.com>
2024-07-02 09:32:23 -0400

    weird formatting

--- src/main.cc
+++ src/main.cc
@@ -15,41 +15,41 @@ int main() {
 	Board board;
 
 	// Add pieces
-    board.addPiece('R', 0, 0);
-    board.addPiece('N', 0, 1);
-    board.addPiece('B', 0, 2);
+	board.addPiece('R', 0, 0);
+	board.addPiece('N', 0, 1);
+	board.addPiece('B', 0, 2);
 	board.addPiece('Q', 0, 3);
 	board.addPiece('K', 0, 4);
 	board.addPiece('B', 0, 5);
 	board.addPiece('N', 0, 6);
-    board.addPiece('R', 0, 7);
-    
+	board.addPiece('R', 0, 7);
+
 	board.addPiece('P', 1, 0);
-    board.addPiece('P', 1, 1);
-    board.addPiece('P', 1, 2);
+	board.addPiece('P', 1, 1);
+	board.addPiece('P', 1, 2);
 	board.addPiece('P', 1, 3);
 	board.addPiece('P', 1, 4);
 	board.addPiece('P', 1, 5);
 	board.addPiece('P', 1, 6);
-    board.addPiece('P', 1, 7);
+	board.addPiece('P', 1, 7);
 
 	board.addPiece('P', 6, 0);
-    board.addPiece('P', 6, 1);
-    board.addPiece('P', 6, 2);
+	board.addPiece('P', 6, 1);
+	board.addPiece('P', 6, 2);
 	board.addPiece('P', 6, 3);
 	board.addPiece('P', 6, 4);
 	board.addPiece('P', 6, 5);
 	board.addPiece('P', 6, 6);
-    board.addPiece('P', 6, 7);
+	board.addPiece('P', 6, 7);
 
 	board.addPiece('r', 7, 0);
-    board.addPiece('n', 7, 1);
-    board.addPiece('b', 7, 2);
+	board.addPiece('n', 7, 1);
+	board.addPiece('b', 7, 2);
 	board.addPiece('q', 7, 3);
 	board.addPiece('k', 7, 4);
 	board.addPiece('b', 7, 5);
 	board.addPiece('n', 7, 6);
-    board.addPiece('r', 7, 7);
+	board.addPiece('r', 7, 7);
 
 	cout << board << endl;
 
