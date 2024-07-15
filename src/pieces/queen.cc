16b13219145d211567fcd1509e894ce82eeb30e4
Andrew Gera <82167983+Andre-The-Greek@users.noreply.github.com>
2024-07-15 16:35:02 -0400

    Rook Queen Bishop calcLegalMoves

--- src/pieces/queen.cc
+++ src/pieces/queen.cc
@@ -7,5 +7,98 @@ Queen::Queen(bool colour, const Posn &posn):
   Piece{colour ? 'Q' : 'q', colour, posn, true, true, true, true} {}
 
 void Queen::calculateLegalMoves(const Board &board) {
-    // Fill in
+    legalMoves.clear();
+    if (board.isPinned(posn)) return;
+    if (vertical) {
+        for (int i = 1; posn.y + i < 8; i++) {
+            if (board[Posn{posn.x, posn.y + i}]) {
+                legalMoves.emplace_back(Posn{posn.x, posn.y + i});
+            } else if (board[Posn{posn.x, posn.y + i}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x, posn.y + i});
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; posn.y - i >= 0; i++) {
+            if (board[Posn{posn.x, posn.y - i}]) {
+                legalMoves.emplace_back(Posn{posn.x, posn.y - i});
+            } else if (board[Posn{posn.x, posn.y - i}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x, posn.y - i});
+                break;
+            } else {
+                break;
+            }
+        }
+    }
+    if (horizontal) {
+        for (int i = 1; posn.x + i < 8; i++) {
+            if (board[Posn{posn.x + i, posn.y}]) {
+                legalMoves.emplace_back(Posn{posn.x + i, posn.y});
+            } else if (board[Posn{posn.x + i, posn.y}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x + i, posn.y});
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; posn.x - i >= 0; i++) {
+            if (board[Posn{posn.x - i, posn.y}]) {
+                legalMoves.emplace_back(Posn{posn.x - i, posn.y});
+            } else if (board[Posn{posn.x - i, posn.y}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x - i, posn.y});
+                break;
+            } else {
+                break;
+            }
+        }
+    }
+    if (positive) {
+        for (int i = 1; posn.x + i < 8 && posn.y + i < 8; i++) {
+            if (board[Posn{posn.x + i, posn.y + i}]) {
+                legalMoves.emplace_back(Posn{posn.x + i, posn.y + i});
+            } else if (board[Posn{posn.x + i, posn.y + i}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x + i, posn.y + i});
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; posn.x - i >= 0 && posn.y - i >= 0; i++) {
+            if (board[Posn{posn.x - i, posn.y - i}]) {
+                legalMoves.emplace_back(Posn{posn.x - i, posn.y - i});
+            } else if (board[Posn{posn.x - i, posn.y - i}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x - i, posn.y - i});
+                break;
+            } else {
+                break;
+            }
+        }
+    }
+    if (negative) {
+        for (int i = 1; posn.x + i < 8 && posn.y - i >= 0; i++) {
+            if (board[Posn{posn.x + i, posn.y - i}]) {
+                legalMoves.emplace_back(Posn{posn.x + i, posn.y - i});
+            } else if (board[Posn{posn.x + i, posn.y - i}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x + i, posn.y - i});
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; posn.x - i >= 0 && posn.y + i < 8; i++) {
+            if (board[Posn{posn.x - i, posn.y + i}]) {
+                legalMoves.emplace_back(Posn{posn.x - i, posn.y + i});
+            } else if (board[Posn{posn.x - i, posn.y + i}]->getColour() != colour) {
+                legalMoves.emplace_back(Posn{posn.x - i, posn.y + i});
+                break;
+            } else {
+                break;
+            }
+        }
+    }
+    vertical = true;
+    horizontal = true;
+    positive = true;
+    negative = true;
 }
